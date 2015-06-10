e5703d0fcff9963ff970bc2f8be31df35b023c16
andygout <andygout@hotmail.co.uk>
2015-06-10 18:55:29 +0100

    Completed: Protractor Step 3 - changing the configuration

--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -16,13 +16,4 @@ describe('Github Profile finder', function() {
     searchButton.click();
     expect(element(by.binding('user.login')).getText()).toEqual('spike01');
   });
-
-  // it('has a title', function() {
-  //   browser.get('http://localhost:8080');
-
-  //   element(by.model('searchCtrl.searchTerm')).sendKeys('spike01');
-  //   element(by.className('btn')).click();
-
-  //   expect(element(by.binding('user.login')).getText()).toEqual('spike01');
-  // });
 });
