ff8848609dffdd7f92ea1a2d2091a3167f0bc260
andygout <andygout@hotmail.co.uk>
2015-06-10 18:49:56 +0100

    Completed: Protractor Step 2 - writing multiple scenarios

--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -1,10 +1,28 @@
 describe('Github Profile finder', function() {
-  it('has a title', function() {
+
+  var searchBox = element(by.model('searchCtrl.searchTerm'))
+  var searchButton = element(by.className('btn'))
+
+  beforeEach(function() {
     browser.get('http://localhost:8080');
+  })
 
-    element(by.model('searchCtrl.searchTerm')).sendKeys('spike01');
-    element(by.className('btn')).click();
+  it('has a title', function() {
+    expect(browser.getTitle()).toEqual('Github user search');
+  });
 
+  it('finds profiles', function() {
+    searchBox.sendKeys('spike01');
+    searchButton.click();
     expect(element(by.binding('user.login')).getText()).toEqual('spike01');
   });
+
+  // it('has a title', function() {
+  //   browser.get('http://localhost:8080');
+
+  //   element(by.model('searchCtrl.searchTerm')).sendKeys('spike01');
+  //   element(by.className('btn')).click();
+
+  //   expect(element(by.binding('user.login')).getText()).toEqual('spike01');
+  // });
 });
